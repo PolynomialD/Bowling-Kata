43af0ded2fff8c41f29a4c9cac6a5c1450aa9c21
PolynomialD <steven.grindey@gmail.com>
2018-10-25 22:49:32 +0100

    refactored a bit

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,43 +1,40 @@
 class BowlingGame {
 
   constructor () {
-    this.score = [...Array(12)].map(x => Array())
-    this.frame = 1
+    this.score = [...Array(10)].map(x => Array())
+    this.frames = 1
     this.bonus = 0
   }
   roll (pins) {
-    this.score[Math.floor(this.frame-1)].push(pins)
-    console.log('initial array', this.score)
+    console.log('frame', this.frames)
+    console.log('bonus',this.bonus)
+    let frame = Math.floor(this.frames-1)
 
-    if(this.bonus > 0 && this.bonus < 3) {
-      this.score[Math.floor(this.frame-1)].push(pins)
-      console.log('array with bonus',this.score)
-      this.bonus -= 1
-      } else { if(this.bonus > 2) {
-        this.score[Math.floor(this.frame-1)].push(pins,pins)
-        this.bonus -= 2
+    this.score[frame].push(pins)
+
+    if(this.bonus > 2) {
+      this.score[frame].push(pins,pins)
+      this.bonus -= 2
+      } else { if(this.bonus > 0) {
+        this.score[frame].push(pins)
+        this.bonus -= 1
+      }
     }
-  }
-  let frameTotal = this.score.reduce((a,n) => {return a.concat(n)}).reduce((a,n) => a+n)
+    let frameTotal = this.score.reduce((a,n) => {return a.concat(n)}).reduce((a,n) => a+n)
     if(pins === 10) {
-      this.bonus += 2
-      this.frame += 0.5
+      this.bonus += 2;
+      if(this.frames < 10) {this.frames += 0.5}
     } else { if(frameTotal === 10) {
       this.bonus += 1
+      }
     }
-  }
-    this.frame += 0.5
+    if(this.frames < 10) this.frames += 0.5
     console.log('score', this.score)
-    console.log('frame', this.frame)
-    console.log('bonus',this.bonus)
-
-
   }
-  getScore () {
-  let flatScore = this.score.reduce((a,n) => {return a.concat(n)})
-  console.log('flatscore array', flatScore)
-  return flatScore.reduce((a,n) => a+n)
-  }
+    getScore () {
+    let flatScore = this.score.reduce((a,n) => {return a.concat(n)})
+    return flatScore.reduce((a,n) => a+n)
+    }
 }
 
 module.exports = BowlingGame
