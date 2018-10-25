9e15dc14d8ed5a5908527550e650fafc29a80b88
PolynomialD <steven.grindey@gmail.com>
2018-10-25 21:51:24 +0100

    4 tests passing

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -9,19 +9,23 @@ class BowlingGame {
     this.score[Math.floor(this.frame-1)].push(pins)
     console.log('initial array', this.score)
 
-    if (this.bonus > 0) {
+    if(this.bonus > 0 && this.bonus < 3) {
       this.score[Math.floor(this.frame-1)].push(pins)
       console.log('array with bonus',this.score)
       this.bonus -= 1
+      } else { if(this.bonus > 2) {
+        this.score[Math.floor(this.frame-1)].push(pins,pins)
+        this.bonus -= 2
     }
-    if (pins === 10) {
+  }
+  let frameTotal = this.score.reduce((a,n) => {return a.concat(n)}).reduce((a,n) => a+n)
+    if(pins === 10) {
       this.bonus += 2
       this.frame += 0.5
-    }
-    let frameTotal = this.score.reduce((a,n) => {return a.concat(n)}).reduce((a,n) => a+n)
-    if (frameTotal === 10) {
+    } else { if(frameTotal === 10) {
       this.bonus += 1
     }
+  }
     this.frame += 0.5
     console.log('score', this.score)
     console.log('frame', this.frame)
