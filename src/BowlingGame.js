a06a95b405101795d2e1e455b10457ecb267434b
PolynomialD <steven.grindey@gmail.com>
2018-10-26 12:00:01 +0100

    refactored

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -2,25 +2,26 @@ class BowlingGame {
 
   constructor () {
     this.score = [...Array(10)].map(x => Array())
-    this.frames = 1
+    this.frame = 1
     this.bonus = 0
   }
-
-  spareCheck (array) {
-    let frameTotal = array.filter((elem,index,self) => {
+  frameTotal (array) {
+    let spareCheck = array.filter((elem,index,self) => {
       return index == self.indexOf(elem)
     })
-    return frameTotal.reduce((a,n) => {return a+n})
+    return spareCheck.reduce((acc,num) => {return acc+num})
+  }
+  getScore () {
+  let flatScore = this.score.reduce((acc,num) => {return acc.concat(num)})
+  return flatScore.reduce((acc,num) => acc+num)
   }
 
   roll (pins) {
-    console.log('frame', this.frames)
-    console.log('bonus',this.bonus)
-    let frame = Math.floor(this.frames-1)
+    let frame = Math.floor(this.frame-1)
 
     this.score[frame].push(pins)
 
-    if(this.bonus > 2) {
+    if(this.bonus > 2) {                        // add bonuses from previous frame
       this.score[frame].push(pins,pins)
       this.bonus -= 2
       } else { if(this.bonus > 0) {
@@ -28,21 +29,15 @@ class BowlingGame {
         this.bonus -= 1
       }
     }
- 
-    if(pins === 10 && this.frames !== 10) {
+    if(pins === 10 && this.frame !== 10) {     // calculate bonuses for next frame
       this.bonus += 2;
-      if(this.frames < 10) {this.frames += 0.5}
-    } else { if(this.spareCheck(this.score[frame]) === 10 && this.frames !== 10) {
+      if(this.frame < 10) this.frame += 0.5
+    } else { if(this.frameTotal(this.score[frame]) === 10 && this.frame !== 10) {
       this.bonus += 1
       }
     }
-    if(this.frames < 10) this.frames += 0.5
-    console.log('score', this.score)
+    if(this.frame < 10) this.frame += 0.5
   }
-    getScore () {
-    let flatScore = this.score.reduce((a,n) => {return a.concat(n)})
-    return flatScore.reduce((a,n) => a+n)
-    }
 }
 
 module.exports = BowlingGame
